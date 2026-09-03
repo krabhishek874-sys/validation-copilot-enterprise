eb52c452f699de153e6cdc485af966b03cbe3dcc
krabhishek874-sys <krabhishek874@gmail.com>
2026-09-03 15:08:35 +0530

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -1,2 +1,36 @@
-def send_notification(recipient,msg):
-    return True
+import smtplib
+from email.mime.text import MIMEText
+from config import *
+
+def send_email(recipient, subject, body):
+
+    try:
+
+        msg = MIMEText(body, "html")
+
+        msg["Subject"] = subject
+        msg["From"] = SMTP_USER
+        msg["To"] = recipient
+
+        server = smtplib.SMTP(
+            SMTP_SERVER,
+            SMTP_PORT
+        )
+
+        server.starttls()
+
+        server.login(
+            SMTP_USER,
+            SMTP_PASSWORD
+        )
+
+        server.send_message(msg)
+
+        server.quit()
+
+        return True
+
+    except Exception as e:
+
+        print(e)
+        return False
