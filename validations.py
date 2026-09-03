783d51f2cc6b3877011899bd0e616df4ab36d670
krabhishek874-sys <krabhishek874@gmail.com>
2026-09-03 14:45:47 +0530

    Update validations.py

--- validations.py
+++ validations.py
@@ -1,2 +1,21 @@
 import streamlit as st
-def show_validations(): st.title("Validations")
+from servicenow_client import get_validations
+
+def show_validations():
+
+    st.title("Validation Records")
+
+    df = get_validations()
+
+    status = st.selectbox(
+        "Filter by Status",
+        ["All", "Failed", "Open"]
+    )
+
+    if status != "All":
+        df = df[df["status"] == status]
+
+    st.dataframe(
+        df,
+        use_container_width=True
+    )
